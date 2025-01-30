8983a5770afde71999dfbe785afe9bae919af57e
Amit Ohol <amitohol9@gmail.com>
2025-01-30 21:56:17 +0530

    tailwind config

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-center text-red-800">
+      hellow world
+    </div>
+  );
+}
+
+export default App;
